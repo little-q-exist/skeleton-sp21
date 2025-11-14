8b71580dd0cc8e1853c4d94054657dc04d226232
little_q_exist <3061982805@qq.com>
2025-11-14 15:28:39 +0800

    LinkListDeque for project1

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,109 @@
+package deque;
+
+public class LinkedListDeque<Item> {
+    class Node {
+        Item data;
+        Node prev;
+        Node next;
+
+        public Node(Item data) {
+            this.data = data;
+            prev = null;
+            next = null;
+        }
+    }
+
+    private Node sentinal;
+    private int size;
+
+    public LinkedListDeque() {
+         sentinal = new Node(null);
+         size = 0;
+    }
+
+    public void addFirst(Item item) {
+        Node newNode = new Node(item);
+        newNode.prev = sentinal;
+        newNode.next = sentinal.next;
+        if (sentinal.next != null) {
+            sentinal.next.prev = newNode;
+        }
+        sentinal.next = newNode;
+    }
+
+    public void addLast(Item item) {
+        Node newNode = new Node(item);
+        newNode.next = sentinal;
+        newNode.prev = sentinal.prev;
+        if (sentinal.prev != null) {
+            sentinal.prev.next = newNode;
+        }
+        sentinal.prev = newNode;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        Node current = sentinal.next;
+        while (current != null && current != sentinal) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public Item removeFirst() {
+        if (sentinal.next == null) {
+            return null;
+        }
+        Item removedData = sentinal.next.data;
+        sentinal.next = sentinal.next.next;
+        sentinal.next.prev = sentinal;
+        size--;
+        return removedData;
+    }
+
+    public Item removeLast() {
+        if (sentinal.prev == null) {
+            return null;
+        }
+        Item removedData = sentinal.prev.data;
+        sentinal.prev = sentinal.prev.prev;
+        sentinal.prev.next = sentinal;
+        size--;
+        return removedData;
+    }
+
+    public Item get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        Node current = sentinal.next;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public Item getRecursive(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        } else {
+            return getRecursiveHelper(index, 0, sentinal.next).data;
+        }
+    }
+
+    private Node getRecursiveHelper(int index, int current, Node node) {
+        if (current == index) {
+            return node;
+        } else {
+            return getRecursiveHelper(index, current + 1, node.next);
+        }
+    }
+}
